4a5d1be1a41dde60d0bbd290c2b13829322855d0
attilav <attila.varjas@gmail.com>
2019-10-23 10:02:55 +0700

    MongoDB setup

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
 // Mount enviroment variables
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -15,9 +20,15 @@ if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-//Mount routers
+// Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold));
+
+// Handle unhandled rejection
+process.on('unhandledRejection', (error, promise) => {
+    console.log(`Error: ${error.message}`.red)
+    server.close(() => process.exit(1));
+});

a181020e875ebf1e1e5cfb2ae9f198e5aad26c69
attilav <attila.varjas@gmail.com>
2019-10-24 17:11:49 +0700

    Filtering, sorting, select fields, pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,8 +7,58 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps});
+    const requestQuery = { ...req.query };
+
+    const fieldsToRemove = ['select', 'sort', 'page', 'limit'];
+
+    fieldsToRemove.forEach(param => delete(requestQuery[param]));
+
+    let queryString = JSON.stringify(requestQuery);
+    queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+    let query = Bootcamp.find(JSON.parse(queryString));
+
+    if(req.query.select) {
+        const select = req.query.select.split(',').join(' ');
+        query = query.select(select);
+    }
+
+    // Sorting
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 100;
+    const startIndex = (page -1) * limit;
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments;
+
+    query = query.skip(startIndex).limit(limit);
+
+    const bootcamps = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    if(endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+
+    if(startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
+
+    res.status(200).json({ success: true, count: bootcamps.length, pagination, data: bootcamps});
 });
 
 // @desc    Get a single bootcamp

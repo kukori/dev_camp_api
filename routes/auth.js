36b2aee6f04507c65616cf8ca354802ac45897fa
attilav <attila.varjas@gmail.com>
2019-10-27 09:54:08 +0700

    Forgot password function

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login, getCurrent } = require('../controllers/auth');
+const { register, login, getCurrent, forgotPassword, resetPassword } = require('../controllers/auth');
 const router = express.Router({ mergeParams: true });
 const { protect } = require('../middlewares/auth');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/forgotpassword').post(forgotPassword);
+router.route('/resetpassword/:resettoken').put(resetPassword);
 router.route('/current').get(protect, getCurrent);
 
 module.exports = router;

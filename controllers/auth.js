aca7e590344f86da545f55652a8bb2acab3b40cc
attilav <attila.varjas@gmail.com>
2019-10-28 04:41:47 +0700

    Logout user

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,17 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
+// @desc    Logout user / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10000),
+        httpOnly: true
+    });
+    res.status(200).json({ success: true, data: {}});
+});
+
 // @desc    Forgot password
 // @route   POST /api/v1/auth/forgotpassword
 // @access  Public
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, getCurrent, forgotPassword, resetPassword, updateUserDetails, updateUserPassword } = require('../controllers/auth');
+const { register, login, logout, getCurrent, forgotPassword, resetPassword, updateUserDetails, updateUserPassword } = require('../controllers/auth');
 const router = express.Router({ mergeParams: true });
 const { protect } = require('../middlewares/auth');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/logout').get(logout);
 router.route('/forgotpassword').post(forgotPassword);
 router.route('/resetpassword/:resettoken').put(resetPassword);
 router.route('/updatedetails').put(protect, updateUserDetails);
